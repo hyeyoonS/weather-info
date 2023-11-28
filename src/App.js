668e000a199d9b52af05117008d32227c4c7ab71
Hyeyoon <foryou9097@gmail.com>
2023-11-28 21:56:59 +0900

    feat : 비동기로 현재 날씨 정보 가져오기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 //1. 앱이 실행되자마자 현재위치기반의 날씨가 보인다.
@@ -8,6 +9,24 @@ import './App.css';
 //6. 데이터를 들고 오는 동안 로딩 스피너가 보인다.
 
 function App() {
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon);
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f4dc587cb12c335777b59a25ea4968d6`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log('data', data);
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
   return <div className="App">TEST</div>;
 }
 
