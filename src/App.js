742eb1ae8a4296fe9d827ca8434fd8517db8fdac
Hyeyoon <foryou9097@gmail.com>
2023-11-28 22:18:39 +0900

    fix : fetch를 axios로 변경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 //1. 앱이 실행되자마자 현재위치기반의 날씨가 보인다.
 //2. 날씨정보: 도시, 섭씨, 화씨, 날씨 상태
@@ -19,9 +20,8 @@ function App() {
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f4dc587cb12c335777b59a25ea4968d6`;
-    let response = await fetch(url);
-    let data = await response.json();
-    console.log('data', data);
+    let response = await axios.get(url);
+    let data = response.data;
   };
 
   useEffect(() => {
