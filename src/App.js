74f4fa47adad6b1a6871fc0a9e75baf0e7c1ba8a
Hyeyoon <foryou9097@gmail.com>
2023-11-29 10:48:23 +0900

    feat : 로딩스피너 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import WeatherBox from './WeatherBox';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherButton from './WeatherButton';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 //1. 앱이 실행되자마자 현재위치기반의 날씨가 보인다.
 //2. 날씨정보: 도시, 섭씨, 화씨, 날씨 상태
@@ -16,6 +17,8 @@ import WeatherButton from './WeatherButton';
 function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(false);
+
   const cities = ['paris', 'new york', 'tokyo'];
 
   const getCurrentLocation = () => {
@@ -28,16 +31,20 @@ function App() {
 
   const getWeatherByCity = async (city) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f4dc587cb12c335777b59a25ea4968d6&units=metric`;
+    setLoading(true);
     let response = await axios.get(url);
     let data = response.data;
     setWeather(data);
+    setLoading(false);
   };
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f4dc587cb12c335777b59a25ea4968d6&units=metric`;
+    setLoading(true);
     let response = await axios.get(url);
     let data = response.data;
     setWeather(data);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -50,10 +57,16 @@ function App() {
 
   return (
     <div className="App">
-      <WeatherContainer>
-        <WeatherBox weather={weather} />
-        <WeatherButton cities={cities} setCity={setCity} />
-      </WeatherContainer>
+      {loading ? (
+        <WeatherContainer>
+          <ClipLoader color="#f88c6b" loading={loading} size={150} aria-label="Loading Spinner" data-testid="loader" />
+        </WeatherContainer>
+      ) : (
+        <WeatherContainer>
+          <WeatherBox weather={weather} />
+          <WeatherButton cities={cities} setCity={setCity} />
+        </WeatherContainer>
+      )}
     </div>
   );
 }
